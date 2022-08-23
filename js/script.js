38295ddb5a155fb0964f4e5abf086ba595dbd839
Lancelot-SO <fsowah001@gmail.com>
2022-08-23 13:45:35 +0000

    new portfolio

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/*=============typing animation========*/
+import Typed from "typed.js"
+var options = {
+    strings: ["","Web Designer","Web Developer","Graphic Designer","Mobile Developer"],
+    typeSpeed: 40,
+    BackSpeed: 60,
+    loop:true
+  };
+var typed = new Typed(".typing",options);
+ 
+
